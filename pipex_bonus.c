18e049f54bef0a35742d1cf17736b0bcab062731
Dwemer Delena <ddelena@er-a1.kzn.21-school.ru>
2021-06-29 16:06:57 +0300

    dv

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -18,9 +18,7 @@ void	do_execve(char **envp, char **argv, int i, int count)
 	j = 0;
 	path = envp[count] + 5;
 	dv = ft_split(path, ':');
-	//for bonus1 argv[2 + i]
-	//for bonus2 argv[3 + i]
-	cmd1 = ft_split(argv[3 + i], ' ');
+	cmd1 = ft_split(argv[2 + i], ' ');
 	while (dv[j])
 	{
 		str = ft_strjoin3(dv[j], "/", cmd1[0]);
@@ -50,29 +48,16 @@ int find_path(char **envp, int argc)
 	return (count);
 }
 
-//buf = NULL;
-//close(fdp[i][0]);
-//while(ft_strncmp(buf, argv[2], ft_strlen(argv[2])))
-//{
-//if(get_next_line(0, &buf) && ft_strncmp(buf, argv[2], ft_strlen(argv[2])))
-//{
-//write(fdp[i][1], buf, ft_strlen(buf));
-//write(fdp[i][1], "\n", 1);
-//}
-//free(buf);
-
-int exec_process(int argc, char **argv, int count, char **envp, int num_argc)
+int exec_process(int argc, char **argv, int count, char **envp, int num_argc, t_arg *fdp)
 {
-	int 	fdp[num_argc][2];
-	int 	pid;
+	int pid;
 	int	i;
 	int fd;
 
 	i = 0;
-	//changed: -1 added
 	while (i < num_argc - 1)
 	{
-		if (pipe(fdp[i]) == -1)
+		if (pipe(fdp[i].pp) == -1)
 		{
 			perror("Error with creating pipe\n");
 			return (1);
@@ -93,44 +78,52 @@ int exec_process(int argc, char **argv, int count, char **envp, int num_argc)
 			int	fd;
 			char *buf;
 
-			if (i == 0) {
-//				fd = open(argv[1], O_RDONLY);
-//				if (fd < 0)
-//				{
-//					perror(argv[1]);
-//					exit(2);
-//				}
-//				if (dup2(fd, STDIN) < 0)
-//					perror("Couldn't read from the file");
-//					//bonus2
-//				else
-//				{
+			if (i == 0)
+			{
+				if (fdp->flag == 1)
+				{
 					buf = NULL;
-					close(fdp[i][0]);
+					close(fdp[i].pp[0]);
 					while(ft_strncmp(buf, argv[2], ft_strlen(argv[2])))
 					{
 						if(get_next_line(0, &buf) && ft_strncmp(buf, argv[2], ft_strlen(argv[2])))
 						{
-							write(fdp[i][1], buf, ft_strlen(buf));
-							write(fdp[i][1], "\n", 1);
+							write(fdp[i].pp[1], buf, ft_strlen(buf));
+							write(fdp[i].pp[1], "\n", 1);
 						}
 						free(buf);
 					}
-//				}
+				}
+				else
+				{
+					fd = open(argv[1], O_RDONLY);
+					if (fd < 0)
+					{
+						perror(argv[1]);
+						exit(2);
+					}
+					if (dup2(fd, STDIN) < 0)
+						perror("Couldn't read from the file");
+				}
 			}
 			if (i != 0)
 			{
-				close(fdp[i - 1][1]);
-				if (dup2(fdp[i - 1][0], STDIN) < 0)
+				close(fdp[i - 1].pp[1]);
+				if (dup2(fdp[i - 1].pp[0], STDIN) < 0)
 					perror("Couldn't read from the pipe1");
-				close(fdp[i - 1][0]);
+				close(fdp[i - 1].pp[0]);
 			}
-			if (dup2(fdp[i][1], STDOUT) < 0)
+			if (dup2(fdp[i].pp[1], STDOUT) < 0)
 				perror("Couldn't write to the pipe1");
-			close(fdp[i][0]);
-			close(fdp[i][1]);
-			//if (i > 0) for bonus2 // add flag
-			if (i > 0)
+			close(fdp[i].pp[0]);
+			close(fdp[i].pp[1]);
+			//if (i > 0) for bonus2 // add flags
+			if (fdp->flag == 1 && i > 0)
+			{
+				do_execve(envp, argv, i + 1, count);
+				perror("child");
+			}
+			else if (fdp->flag != 1)
 			{
 				do_execve(envp, argv, i, count);
 				perror("child");
@@ -140,14 +133,12 @@ int exec_process(int argc, char **argv, int count, char **envp, int num_argc)
 		else
 		{
 			wait(0);
-			close((fdp[i][1]));
+			close((fdp[i].pp[1]));
 			if (i)
-				close(fdp[i - 1][0]);
+				close(fdp[i - 1].pp[0]);
 		}
 		i++;
 	}
-//	int fd;
-
 	fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 00774);
 	if (fd < 0)
 	{
@@ -156,10 +147,10 @@ int exec_process(int argc, char **argv, int count, char **envp, int num_argc)
 	}
 	if (dup2(fd, STDOUT) < 0)
 		perror("Couldn't read from the file");
-	if (dup2(fdp[num_argc - 2][0], STDIN) < 0)
+	if (dup2(fdp[num_argc - 2].pp[0], STDIN) < 0)
 		perror("Couldn't read from the pipe2");
-	close(fdp[num_argc - 2][1]);
-	close(fdp[num_argc - 2][0]);
+	close(fdp[num_argc - 2].pp[1]);
+	close(fdp[num_argc - 2].pp[0]);
 	do_execve(envp, argv, i, count);
 	perror("parent");
 	return (0);
@@ -169,10 +160,11 @@ int main(int argc, char **argv, char **envp)
 {
 	int		i;
 	int		count;
-
+	t_arg	*fdp;
 	int		num_argc;
 
 	num_argc = argc - 3;
+	fdp =  (t_arg *)malloc(sizeof (t_arg) * argc);
 	i = 0;
 	count = find_path(envp, argc);
 	if (count == -1)
@@ -189,9 +181,10 @@ int main(int argc, char **argv, char **envp)
 			perror("Not enough arguments");
 			exit(EXIT_FAILURE);
 		}
+		fdp->flag = 1;
 		num_argc = argc - 4;
 	}
-	if (exec_process(argc, argv, count, envp, num_argc))
+	if (exec_process(argc, argv, count, envp, num_argc, fdp))
 		return (2);
 	//segfault ./a.out
 	//seeking in current directory
